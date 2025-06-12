c3f8ded70d2d9baa8cd9dfc8b6d202a87bd6f671
Fikiswa Matoti <zulu-matoti@outlook.com>
2025-06-12 13:54:28 +0200

    Commented out the entire code.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,11 @@
-let js = "amazing";
+/*let js = "amazing";
 
 console.log(40+8+23-10);
 
 console.log("Jonas");
 console.log("23");
 
+// Variable name conventions
 let firstName = "Fikiswa";
 console.log(firstName);
 console.log(firstName);
@@ -13,4 +14,4 @@ console.log(firstName);
 let myFirstJob = "Intern_Administrator";
 let myCurrentJob = "Teacher"
 
-console.log(myFirstJob);
+console.log(myFirstJob);*/
